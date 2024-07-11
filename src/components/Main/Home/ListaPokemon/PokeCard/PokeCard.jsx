1c8a441f365c5f8961a431c1075f197fca99a11d
steph-d989 <steb_d@hotmail.com>
2024-07-11 20:25:03 +0200

    scss listo

--- src/components/Main/Home/ListaPokemon/PokeCard/PokeCard.jsx
+++ src/components/Main/Home/ListaPokemon/PokeCard/PokeCard.jsx
@@ -1,39 +1,24 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import React from 'react';
+import '../PokeCard';
 
 export default function PokeCard({ pokemon: { name, id, sprites, types } }) {
   console.log(name, id, sprites, types);
   let name1 = name;
-  name1= name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase();
+  name1 = name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase();
   const id1 = id;
   const image1 = sprites.other["official-artwork"].front_default;
   const type1 = types[0].type.name;
   const type2 = types.length > 1 ? types[1].type.name : null;
 
+  const typeClass = `type-${type1}`;
+
   return (
-    <Card sx={{ maxWidth: 200 }}>
-      <CardContent>
-        <Typography variant="body2" /* color="text.secondary" */>
-          nº{id1}
-        </Typography>
-        <Typography variant="body2" /* color="text.secondary" */>
-          {name1}
-        </Typography>
-        <Typography variant="body2" /* color="text.secondary" */>
-          {type1}
-        </Typography>
-        {type2!==null&&<Typography variant="body2" /* color="text.secondary" */>
-          {type2}
-        </Typography>}
-      </CardContent>
-      <CardMedia
-        sx={{ height: 100, width: 100 }}
-        image={image1}
-        title={name1}
-      />
-    </Card>
+    <div className={`pokeCard ${typeClass}`}>
+      <div className="pokeId">nº{id1}</div>
+      <div className="pokeName">{name1}</div>
+      <img src={image1} alt={name1} />
+      <div className="pokeType">{type1}</div>
+      {type2 && <div className="pokeType">{type2}</div>}
+    </div>
   );
 }
